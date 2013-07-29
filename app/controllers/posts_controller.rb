fa5ec44d41edc4c809c211d127199d1ea4a09b42
lucasjmoore <lucasmoore88@gmail.com>
2013-07-29 15:44:40 -0400

    added navigtion for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,16 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      redirect_to edit_post_path(@post)
+    end
   end
 
   def new
@@ -19,9 +26,13 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
+    redirect_to posts_path
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
