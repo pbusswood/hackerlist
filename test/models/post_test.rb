92dc22035cb57cd9f15c40fb470399ea86ffc52e
Jamie Franklin <jamie@intertitles.com>
2013-07-29 18:49:13 +0000

    Validations and testing for posts added

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "that a post requires a title" do
+    post = Post.new
+    assert !post.save
+    assert !post.errors[:title].empty?
+  end
+
+  test "that a post requires content" do
+    post = Post.new
+    assert !post.save
+    assert !post.errors[:content].empty?
+  end
+
+  test "that a post content is at least 2 characters" do
+    post = Post.new
+    post.content = "J"
+    assert !post.save
+    assert !post.errors[:content].empty?
+  end
+
+  test "that a post requires an email" do
+    post = Post.new
+    assert !post.save
+    assert !post.errors[:email].empty?
+  end
+
+  test "that a post requires a correctly formatted email" do
+    post = Post.new
+    post.email = "980765dnknakjan.com@"
+    assert !post.save
+    assert !post.errors[:email].empty?
+  end
+
+  test "that a post requires a location" do
+    post = Post.new
+    assert !post.save
+    assert !post.errors[:location].empty?
+  end
 end
